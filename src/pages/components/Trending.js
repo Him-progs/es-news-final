6490aa57c7696290471cd31d4448892f6da9da06
himanshu <himanshu.dodwani@cloudframe.com>
2023-05-20 19:45:50 +0530

    slider-responsive

--- src/pages/components/Trending.js
+++ src/pages/components/Trending.js
@@ -13,7 +13,33 @@ const Trending = () => {
     arrows: true,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+        {
+            breakpoint: 420,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+            }
+          },
+    ]
   };
 
   useEffect(() => {
